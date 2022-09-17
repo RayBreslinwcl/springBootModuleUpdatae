e591bf308dd582aed611d28871a33140b6ea806c
ray <2728928100@qq.com>
2022-09-17 15:11:31 +0800

    依据新环境修改一些参数

--- springboot-shiro3-hm/sql/sql.sql
+++ springboot-shiro3-hm/sql/sql.sql
@@ -1,5 +1,9 @@
 create table user_shiro(
- id int primary key auto_increment,
- name varchar(20),
- password varchar(30)
+                           id int primary key auto_increment,
+                           name varchar(20),
+                           password varchar(30),
+                           perms varchar(20)
 )
+
+
+INSERT INTO `test`.`user_shiro`( `name`, `password`,`perms`) VALUES ( 'admin', '123456','user:update');
