d2044f66bcbd380ff043fbf72cd49df41684e9c7
ray <2728928100@qq.com>
2024-06-18 17:37:46 +0800

    总结

---
+++ springboot-quickstart/Readme.md
@@ -0,0 +1,5 @@
+# 一、postman测试接口
+
+### 1.文章总结：04-网络注解-2-postman测试post和get请求.md
+
+[finished]
--- springboot-quickstart/src/main/java/com/rhwayfun/springboot/quickstart/web/DemoController.java
+++ springboot-quickstart/src/main/java/com/rhwayfun/springboot/quickstart/web/DemoController.java
@@ -2,6 +2,8 @@ package com.rhwayfun.springboot.quickstart.web;
 
 import com.rhwayfun.springboot.quickstart.pojo.Dog;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by chubin on 2017/9/10.
  */
 @RestController
+@RequestMapping("/api")
 public class DemoController {
 
     @Autowired
@@ -23,4 +26,11 @@ public class DemoController {
     public String dog(){
         return dog.toString();
     }
+
+    @PostMapping("/create")
+    public Dog createUser(@RequestBody Dog dogReq) {
+        // ... 创建用户信息的逻辑
+        return dogReq;
+    }
+
 }
