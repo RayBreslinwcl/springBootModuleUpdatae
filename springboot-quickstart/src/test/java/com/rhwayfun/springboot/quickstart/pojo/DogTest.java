90f489994b7c4b7368e8fba0aa8dc791b565908d
ray <2728928100@qq.com>
2024-06-27 18:54:43 +0800

    user service

--- springboot-quickstart/src/test/java/com/rhwayfun/springboot/quickstart/pojo/DogTest.java
+++ springboot-quickstart/src/test/java/com/rhwayfun/springboot/quickstart/pojo/DogTest.java
@@ -1,11 +1,13 @@
 package com.rhwayfun.springboot.quickstart.pojo;
 
+import com.google.common.collect.Maps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(Dog.class)
@@ -16,5 +18,14 @@ public class DogTest {
     @Test
     public void contextLoads(){
         System.out.println(dog);
+    }
+
+    @Test
+    public void testonly(){
+        final HashMap<String, String> objectObjectHashMap = Maps.newHashMapWithExpectedSize(1);
+        objectObjectHashMap.put("key1","value1");
+        objectObjectHashMap.clear();
+        objectObjectHashMap.put("key2","value1");
+        System.out.println(objectObjectHashMap);
     }
 }
