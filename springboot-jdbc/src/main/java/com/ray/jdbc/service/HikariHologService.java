49297ea95ea67f9430ae626dce6b35c27e166597
ray <2728928100@qq.com>
2024-06-27 18:54:27 +0800

    user service

--- springboot-jdbc/src/main/java/com/ray/jdbc/service/HikariHologService.java
+++ springboot-jdbc/src/main/java/com/ray/jdbc/service/HikariHologService.java
@@ -3,6 +3,8 @@ package com.ray.jdbc.service;
 import com.ray.jdbc.entity.OrderDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -16,7 +18,11 @@ import java.util.Map;
  * @Created: 2024/6/17 18:22
  */
 @Service
+//@ConditionalOnProperty(name = "spring.datasource.mysql.active", havingValue = "true")
 public class HikariHologService {
+
+    @Value("${spring.datasource.mysql.username}")
+    private String mysqlUsername; //root
 
     @Autowired
     @Qualifier("hologJdbcTemplate")
@@ -24,7 +30,7 @@ public class HikariHologService {
 
     public List<Map<String, Object>> getAllData(){
         // 创建动态的占位符
-
+        System.out.println(mysqlUsername);
         // 构建查询语句
         String sql = String.format("SELECT * FROM %s", "public.segment_detail");
 //        String sql = String.format("SELECT * FROM %s where 1=2", "public.segment_detail");//可以的为了测试 空
